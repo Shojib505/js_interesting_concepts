c9f7234a2c44b2a4594f7cdf98c71af963e7a86e
alish <alishojib12@gmail.com>
2022-05-20 15:32:35 +0600

    closure conpects clear

--- closure.js
+++ closure.js
@@ -5,6 +5,12 @@ function stopWatch() {
         return counter;
     }
 }
+
+//when you call clock 1 it's create it's own function 
 let clock1 = stopWatch();
+/*if you call a funtion0 which one is alson return another funtion1
+then if that funtion1 has a varibale which is declared in function0 then
+ it will resarverd that varibal inside him
+ and it will create it's own closer/encapsulaion/prvater reffarance*/
 console.log(clock1());
 console.log(clock1());
